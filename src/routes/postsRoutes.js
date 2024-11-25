21ffe666444714458d523003989857c9df343e6b
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2024-11-24 23:46:57 -0400

    ✨ feat: Criaçao do put para atualizaçao de posts e integracao com a api da gemini

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import cors from "cors";
+import { atualizarNovoPost, listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+};
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -9,19 +15,23 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         cb(null, file.originalname);
     }
-})
+});
 
 const upload = multer({ dest: "./uploads" , storage})
 
 
 const routes = (app) => {
     app.use(express.json());
+
+    app.use(cors(corsOptions));
     
     app.get("/posts", listarPosts);
 
     app.post("/posts", postarNovoPost);
 
     app.post("/upload", upload.single("imagem"), uploadImagem);
+
+    app.put("/upload/:id", atualizarNovoPost);
 
     // app.get("/posts/:id", listarPostsID);
     
